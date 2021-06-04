62b9998d0d5f76f5399e75f73b4f3fb85263db3d
Fx Aminul <84038131+aminul240@users.noreply.github.com>
2021-06-05 01:37:35 +0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,21 @@
+"use strict"
+
+
+let user = [
+    "aminul",
+    "samim",
+    "minu",
+    "mehedi",
+    "shibu",
+];
+
+let arrLength = user.length;
+
+console.log(arrLength);
+
+let counter;
+
+for (counter = 0; counter < arrLength; counter++) {
+
+    console.log(user[counter]);
+}
